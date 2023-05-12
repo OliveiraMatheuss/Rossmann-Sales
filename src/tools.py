8bdfe6633cdfd67bfc8c7fee3202bcbacda9e512
Matheus de Oliveira <ramos.matheus@engenharia.ufjf.br>
2023-05-12 09:20:03 -0300

    Adicionado função def jupyter_setting ao tools.py

--- src/tools.py
+++ src/tools.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 from scipy import stats  as ss
+import seaborn as sns
+from matplotlib import pyplot as plt
+from IPython.core.display import HTML
 
 
 
@@ -16,3 +19,16 @@ def cramer_v( x, y ):
     rcorr = r - (r-1)**2/(n-1)
     
     return np.sqrt( (chi2corr/n) / ( min( kcorr-1, rcorr-1 ) ) )
+
+def jupyter_settings():
+
+    plt.style.use( 'bmh' )
+    plt.rcParams['figure.figsize'] = [25, 12]
+    plt.rcParams['font.size'] = 24
+    
+    display( HTML( '<style>.container { width:100% !important; }</style>') )
+    pd.options.display.max_columns = None
+    pd.options.display.max_rows = None
+    pd.set_option( 'display.expand_frame_repr', False )
+    
+    sns.set()
